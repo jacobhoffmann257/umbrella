d6642fada0a7bcd836c0b7510d51216b2e93610f
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-06 00:56:23 +0000

    pirate set

--- Umbrella.rb
+++ Umbrella.rb
@@ -7,17 +7,13 @@ puts "Where are you?"
 
 location = gets.chomp
 puts "#{location}"
+#parsing gmaps
 gmaps_key = ENV.fetch("GMAPS_KEY")
 gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{gmaps_key}"
-
-#gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{gmaps_key}"
 raw_gmaps = HTTP.get(gmaps_url)
 parsed_gmaps = JSON.parse(raw_gmaps)
-puts parsed_gmaps
-
 gmaps_array = parsed_gmaps.fetch("results");
 first_hash_results = gmaps_array.at(0)
-
 geo_hash = first_hash_results.fetch("geometry")
 location_hash = geo_hash.fetch("location")
 long = location_hash.fetch("lng")
